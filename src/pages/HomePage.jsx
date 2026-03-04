70a0f5dceef6787f90b182ae51ebffe700f17017
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2026-03-04 18:38:41 +0100

    update on code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -119,8 +119,8 @@ const HomePage = ({ navigate }) => {
               >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
               </svg>
-            </Link>
-          </div>
+            </div>
+          </Link>
         </div>
       </div>
 
@@ -162,7 +162,6 @@ const HomePage = ({ navigate }) => {
                 </span>
               )}
             </button>
-            </div>
           </div>
           <div className="flex gap-2">
             {/* ADD BUTTON - navigates to Create Event */}
